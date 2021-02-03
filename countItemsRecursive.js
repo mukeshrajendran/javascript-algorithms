117ef0b97f72ae723805860e12b4893e8803d459
mukeshrajendran <58806133+mukeshrajendran@users.noreply.github.com>
2021-02-03 13:28:52 +0530

    Update countItemsRecursive.js

--- countItemsRecursive.js
+++ countItemsRecursive.js
@@ -1,10 +1,10 @@
-function countItems(list){
-    if(!list[0]){
+function countItems(list) {
+    if (!list[0]) {
         return 0;
-    }else{
+    } else {
         return 1 + countItems(list.slice(1))
     }
-    
+
 }
 
-let result = countItems([2,2,1000,1]));
+let result = countItems([2, 2, 1000, 1]);
