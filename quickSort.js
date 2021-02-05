bba51aba95e21e7f7cfdd9ee8ff0b83353e8d510
mukeshrajendran <58806133+mukeshrajendran@users.noreply.github.com>
2021-02-06 01:08:01 +0530

    Update quickSort.js

--- quickSort.js
+++ quickSort.js
@@ -1,40 +1,38 @@
+function pivot(arr, start=0, end=arr.length - 1) {
+    const swap = (arr,idx1,idx2)=>{
+        [arr[idx1],arr[idx2]] = [arr[idx2], arr[idx1]];
+    }
+    ;
 
-function pivot(arr, start = 0, end = arr.length - 1) {
-    const swap = (arr, idx1, idx2) => {
-      [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
-    };
-  
     // We are assuming the pivot is always the first element
     let pivot = arr[start];
     let swapIdx = start;
-  
+
     for (let i = start + 1; i <= end; i++) {
-      if (pivot > arr[i]) {
-        swapIdx++;
-        console.log(swapIdx,i)
-        swap(arr, swapIdx, i);
-      }
+        if (pivot > arr[i]) {
+            swapIdx++;
+            console.log(swapIdx, i)
+            swap(arr, swapIdx, i);
+        }
     }
-    console.log(arr)
-  
+
     // Swap the pivot from the start the swapPoint
     swap(arr, start, swapIdx);
-    console.log(arr)
 
     return swapIdx;
-  }
-  
-  
-  function quickSortInplace(arr, left = 0, right = arr.length -1){
-      if(left < right){
-          let pivotIndex = pivot(arr, left, right) //3
-          //left
-          quickSortInplace(arr,left,pivotIndex-1);
-          //right
-          quickSortInplace(arr,pivotIndex+1,right);
-        }
-       return arr;
-  } 
+}
+
+function quickSortInplace(arr, left=0, right=arr.length - 1) {
+    if (left < right) {
+        let pivotIndex = pivot(arr, left, right)
+        //3
+        //left
+        quickSortInplace(arr, left, pivotIndex - 1);
+        //right
+        quickSortInplace(arr, pivotIndex + 1, right);
+    }
+    return arr;
+}
 
 function quickSorts(list) {
     let pivot = list[0];
@@ -53,4 +51,4 @@ function quickSorts(list) {
     return quickSorts(smaller).concat([pivot]).concat(quickSorts(greater));
 }
 
-quickSortInplace([-3, 2, 4, 6, 9,100, 1, 2, 5, 3, 23])
+quickSortInplace([-3, 2, 4, 6, 9, 100, 1, 2, 5, 3, 23])
