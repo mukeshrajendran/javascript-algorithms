d9e21146e82feaf2577dd819d599ef4af12fd528
mukeshrajendran <58806133+mukeshrajendran@users.noreply.github.com>
2021-02-10 13:11:25 +0530

    Update dijkstrasAlgorithm.js

--- dijkstrasAlgorithm.js
+++ dijkstrasAlgorithm.js
@@ -9,41 +9,61 @@ function findLowCostNode(costs, processed) {
     }
     return node;
 }
-function dijkstra(graph, start, end, costs, parents) {
+
+function initialCost(node,end){
+    let initialCost = {};
+    for(let key in node){
+        initialCost[key] = node[key];
+    }
+    if(!initialCost[end]) initialCost[end] = Infinity;
+    return initialCost; 
+}
+
+function initialParent(node,start,end){
+    let initialParent = {};
+    for(let key in node){
+        initialParent[key] = start;
+    }
+    if(!initialParent[end]) initialParent[end] = null;
+    return initialParent;
+
+}
+
+function dijkstra(graph, start,end) {
+    let costs = initialCost(graph[start],end);
+    let parents = initialParent(graph[start],start,end);
     let processed = []
     let node = findLowCostNode(costs, processed);
     let neighbours = null;
-    while (node) {
+    while(node){
         neighbours = graph[node];
-        for (let key in neighbours) {
-            if (costs[key] > (costs[node] + graph[node][key])) {
-                costs[key] = costs[node] + graph[node][key];
-                parents[key] = node;
-            }
+        for(let key in neighbours){
+            costs[key] = costs[key] || Infinity;
+           if(costs[key]> (costs[node]+graph[node][key])){
+               costs[key] = costs[node]+graph[node][key];
+               parents[key] = node;
+           }
         }
         processed.push(node);
-        node = findLowCostNode(costs, processed);
+        node = findLowCostNode(costs,processed);
     }
-    return {
-        costs,
-        parents
-    };
+    return {costs,parents};
 
 }
 
 let graph = {};
-graph['a'] = {}
-graph['a']['fin'] = 1
-graph['b'] = {}
-graph['b']['a'] = 3
-graph['b']['fin'] = 5
-graph['fin'] = {}
-costs = {}
-costs['a'] = 6
-costs['b'] = 2
-costs['fin'] = Infinity;
-parents = {}
-parents['a'] = 'start'
-parents['b'] = 'start'
-parents['fin'] = null
-let result = dijkstra(graph, 'a', 'fin', costs, parents);
+graph['a'] = {};
+graph['a']['b'] = 4;
+graph['a']['c'] = 2;
+graph['b'] = {};
+graph['b']['e'] = 3;
+graph['c'] = {};
+graph['c']['d'] = 2;
+graph['c']['f'] = 4;
+graph['d'] = {};
+graph['d']['e'] = 3;
+graph['d']['f'] = 1;
+graph['e'] = {};
+graph['e']['f'] = 3;
+graph['f'] = {};
+let result = dijkstra(graph, 'a', 'e');
